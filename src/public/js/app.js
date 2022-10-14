fd533e41d08954af3b878f32bab9a0cc5711eb78
normalstory <everpresent@hanmail.net>
2022-10-14 12:03:13 +0900

    1)웹소켓 설치 2)FtoB 연결을 위한 환결 설정, 기본 메시지 송수신, 결과 확인.

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,19 @@
-alert("hello world");
+// alert("hello world");
+
+// ws에 연결을 요청하고, 받을 오브젝트 선언 
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", ()=>{            // 샘플1-2. 웹소캣 연결 대기& 확인 
+    console.log("Connected to Server");
+})
+socket.addEventListener("message", (message)=>{  // 샘플2-2. 메시지 수신 대기& 확인 
+    console.log("Just got this : ", message.data);
+    
+})
+socket.addEventListener("close", ()=>{
+    console.log(" Disconnect from the Server ");
+})
+
+setTimeout( () => {
+socket.send("HelLo Server? from the Browser!");  // 샘플 3-1 이번엔 브라우저에서 먼저 메시지를 보내는 경우 
+}, 10000);
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -6,6 +8,24 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req,res) => res.render("home"));
+app.get("/*",(req,res) => res.redirect("/")); //catchall url 설정
 
-const handleListen = () => console.log('listening on http://localhost:3000');
-app.listen(3000, handleListen);
+const handleListen = () => console.log('listening on http://localhost:3000'); // ws://localhost:3000
+// app.listen(3000, handleListen);
+
+// express(http용)에 ws도 함께 사용할 수 있도록 서버를 객체로 선언 
+const server = http.createServer(app); // http server 
+const wss = new WebSocket.Server({server}) // http server를 재사용하는 WebSocket server 
+
+wss.on("connection", (socket)=>{             // 샘플 1-1. 웹소켓 연결  
+    console.log("Connected to Browser");
+    socket.on("close", ()=>console.log("Disconnected from the Browser"))  // Browser 탭 또는 창이 닫힐때 실행 
+  
+    socket.send("Hello Client!");            // 샘플 2-1. 메시지 송신  
+    socket.on("message", (message) => {      // 샘플 3-2. 이번엔 클라이언트로 부터 메시지 수신
+        // console.log(message);  //인코딩에러
+        console.log(message.toString('utf8')); // 샘플 3-2. + 인코딩 수정 
+    }) 
+})
+
+server.listen(3000,handleListen);
