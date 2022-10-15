e789b0081c773bf054b6727419f514be0d6526f9
normalstory <everpresent@hanmail.net>
2022-10-15 22:50:08 +0900

    방 생성 by socket.emit("이벤트",{argMsg},()=>{lastArg});

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,16 @@
 const socket = io();
+
+const welcome = document.querySelector("#welcome");
+const form = document.querySelector("form");
+
+function handleRoomSubmit(event){
+    event.preventDefault();
+    const input = form.querySelector("input");
+    socket.emit("enter_room",{payload:input.value},()=>{
+        console.log(`This func that the last argument for socket.emit() 
+                     that executed by F after call by B`);
+    });
+    input.value="";
+}
+
+form.addEventListener("submit", handleRoomSubmit);
